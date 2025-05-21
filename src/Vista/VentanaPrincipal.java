014c50fb978f9c9f28af9bada493a3d3417d7182
Erick González <erickgoonzalez5@gmail.com>
2025-05-20 19:31:35 -0600

    Cree la estrutura de el toolbar y el menu sin funcionalidad

--- src/Vista/VentanaPrincipal.java
+++ src/Vista/VentanaPrincipal.java
@@ -1,4 +1,76 @@
 package Vista;
 
-public class VentanaPrincipal {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class VentanaPrincipal extends JFrame {
+
+    JDesktopPane desktopPane;
+    JMenu menuVehiculos;
+    JMenuItem itemAltas, itemBajas, itemCambios, itemConsultas;
+    JButton btnToolAgregar, btnToolEliminar, btnToolCambiar, btnToolConsultar;
+    JInternalFrame IF_Altas, IF_Bajas, IF_Cambios, IF_Consultas;
+
+
+    public VentanaPrincipal() {
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Sistema Distribuidor de Autos Amistosos");
+        setSize(800, 700);
+        setLocationRelativeTo(null);
+
+        desktopPane = new JDesktopPane();
+        desktopPane.setBackground(new Color(0, 150, 163));
+        add(desktopPane);
+
+        JMenuBar menuBar = new JMenuBar();
+        menuVehiculos = new JMenu("Vehiculos");
+
+        itemAltas = new JMenuItem("Agregar");
+        itemBajas = new JMenuItem("Eliminar");
+        itemCambios = new JMenuItem("Modificar");
+        itemConsultas = new JMenuItem("Consultar");
+
+
+
+        menuVehiculos.add(itemAltas);
+        menuVehiculos.add(itemBajas);
+        menuVehiculos.add(itemCambios);
+        menuVehiculos.add(itemConsultas);
+
+        menuBar.add(menuVehiculos);
+        setJMenuBar(menuBar);
+
+        JToolBar toolBar = new JToolBar("Herramientas");
+        btnToolAgregar = new JButton("Agregar");
+        btnToolEliminar = new JButton("Eliminar");
+        btnToolCambiar = new JButton("Modificar");
+        btnToolConsultar = new JButton("Consultar");
+
+
+
+        toolBar.add(btnToolAgregar);
+        toolBar.add(btnToolEliminar);
+        toolBar.add(btnToolCambiar);
+        toolBar.add(btnToolConsultar);
+
+        getContentPane().add(toolBar, BorderLayout.NORTH);
+        getContentPane().add(desktopPane, BorderLayout.CENTER);
+
+
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                new VentanaPrincipal();
+            }
+        });
+    }
 }
